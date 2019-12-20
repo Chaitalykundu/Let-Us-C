7eff2a05ac8b16997e11e6805c3df8a53f36cdc5
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-12-20 19:53:37 +0530

    distance in multi unit

---
+++ distance.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    float km, m, f, i, c;
+    printf("\Enter the distance in km: ");
+    scanf("%f",&km);
+    m= 1000 * km;
+    f= 3.28084 * m;
+    i= 12 *f;
+    c=1000000* km;
+    printf("\ndistance in metre: %ff", m);
+    printf("\ndistance in feet: %f", f);
+    printf("\ndistance in inches: %f", i);
+    printf("\ndistance in centimetre: %f", c);
+    return 0;
+}
