5b821a4c7ad4edc2bd65f93d93fdf35841f2b682
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-06-28 18:56:07 +0530

    Add files via upload

---
+++ Chapter 4/greatest no using conditional operator.c
@@ -0,0 +1,11 @@
+/* Find greatest of three numbers using conditional operator */
+
+#include<stdio.h>
+int main()
+{
+    int a,b,c, greatest;
+    printf("Enter the three numbers: ");
+    scanf("%d %d %d",&a, &b, &c);
+    greatest=(a>b)?((a>c)?a:c):(b>c?b:c);
+    printf("Greatest no= %d",greatest);
+}
