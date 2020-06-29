47a53668af18e3cbe242935d25f37b809776e70f
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-06-29 23:17:23 +0530

    Add files via upload

---
+++ chapter 5/A . Example/loop control 4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    while('a'<'b')
+        printf("Malayalam is a palindrome\n");
+    return 0;
+}
+
+/* o/p: Malayalam is a palindrome
+        Malayalam is a palindrome
+        Malayalam is a palindrome
+        Malayalam is a palindrome
+        Malayalam is a palindrome
+        .
+        .
+        .
+         infinity loop*/
