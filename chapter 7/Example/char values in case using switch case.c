bd1dc9e03dbcbf2bd844469e2f476df6ec4d8cad
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-09 14:54:02 +0530

    Add files via upload

---
+++ chapter 7/Example/char values in case using switch case.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+
+    printf("Enter any alphabet: ");
+    scanf("%c",&ch);
+    switch(ch)
+    {
+        case 'a':
+            printf("\nI'm Case a");
+            break;
+        case 'A':
+            printf("\nI'm Case A");
+            break;
+        case 'C':
+            printf("\nI'm Case C");
+            break;
+        default:
+            printf("\nI'm in default");
+
+    }
+}
