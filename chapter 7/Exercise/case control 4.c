5d32cb37a4daa748454820b4d476682914cead20
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-10 16:39:52 +0530

    Add files via upload

---
+++ chapter 7/Exercise/case control 4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int k;
+    float j=2.0;
+    switch(k=j+1)
+    {
+        case 3:
+            printf("Trapped\n");
+            break;
+        default:
+            printf("Caught!\n");
+    }
+    return 0;
+}
+
+/* o/p: Trapped
+*/
