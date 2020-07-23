6dfba70a80cfc07237e85e92e72b53888a75ee94
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-07-23 18:21:13 +0530

    average marks of 10 students

---
+++ chapter 13/Example/average marks.c
@@ -0,0 +1,18 @@
+/* Write a program to find the average marks obtained by a class of 10 students in a test */
+
+#include<stdio.h>
+int main()
+{
+    int a[10], i, sum=0;
+    float avg;
+    printf("Enter marks: ");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<10;i++)
+    sum = sum + a[i];
+    avg = sum / 10;
+    printf("Average marks= %f",avg);
+}
