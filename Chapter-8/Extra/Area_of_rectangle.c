fa457a8f3e077758332c6db2dbff10bea13120bb
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-11-06 19:15:48 +0530

    Add file

---
+++ Chapter-8/Extra/Area_of_rectangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+// function prototype declaration
+int AreaofRectangle(int length, int breadth);
+
+// main function
+int main()
+{
+    int l,b, area;
+    printf("Enter the length and breadth: ");
+    scanf("%d %d", &l,&b);
+    area= AreaofRectangle(l,b); // function call
+    printf("Area of a rectangle is %d", area);
+    return 0;
+}
+
+
+// function definition
+int AreaofRectangle(int length, int breadth)
+{
+    int area;
+    area= length * breadth;
+    return area;
+}
