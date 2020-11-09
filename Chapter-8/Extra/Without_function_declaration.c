dd4e0b200a1892502cd0d967d9d35abfee92d396
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2020-11-09 23:41:07 +0530

    Add file

---
+++ Chapter-8/Extra/Without_function_declaration.c
@@ -0,0 +1,20 @@
+/* Author: Chaitaly Kundu
+Date; 09-11-2020 */
+
+/* Write a program without declaring function */
+
+#include<stdio.h>
+
+// function definition
+char fun()
+{
+    printf("\nHello viewer");
+}
+
+// main function
+int main()
+{
+    fun();
+    printf("\nWelcome to declaration of function");
+    return 0;
+}
