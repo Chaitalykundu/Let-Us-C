76f954d61ee616428a52dc4bd4830316e742f011
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-18 13:13:21 +0530

    Add commit

--- Chapter-1/Exercise-F/CPP/Paper_size.cpp
+++ Chapter-1/Exercise-F/CPP/Paper_size.cpp
@@ -1,11 +1,15 @@
 /* Author : Chaitaly Kundu
-Date: 1st March, 2021 */
+Date: 18-04-21 */
 
 /* Paper of size A0 has dimensions 1189 mm * 841 mm.
    Each subsequent size A(n) is defined as A(n-1) cut in half parallel to its shorter sides.
    Thus paper of size A1 would have dimensions 841 mm * 594 mm.
    Write a program to calculate and print paper sizes A0, A1, A2 ... A8. */
-#include<stdio.h>
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
 
 /* main function */
 int main()
@@ -13,7 +17,7 @@ int main()
     int length=1189, breadth=841, past_length, present;
     for(present=0;present<=8;present++)
     {
-        printf("\npaper A%d :size %d mm *%d mm",present, length,breadth);
+        cout << "\npaper A" << present << " and size " << length << "mm * " << breadth << "mm" ;
         past_length=length;
         length=breadth;
         breadth=past_length/2;
--- Chapter-1/Exercise-F/CPP/Rectangle_Circle.cpp
+++ Chapter-1/Exercise-F/CPP/Rectangle_Circle.cpp
@@ -1,5 +1,6 @@
 /* Author : Chaitaly Kundu
-Date: 1st March, 2021 */
+Date: 18-04-21 */
+
 
 /* The length and breadth of a rectangle and radius of a circle are input through the keyboard.
    Write a program to calculate the area and perimeter of the rectangle , and the area and circumference of the circle. */
