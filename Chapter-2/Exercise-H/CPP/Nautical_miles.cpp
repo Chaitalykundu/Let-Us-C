d3a4b401d514a9910e5f31fb414d986ed9663603
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-19 12:37:04 +0530

    Add commit

---
+++ Chapter-2/Exercise-H/CPP/Nautical_miles.cpp
@@ -0,0 +1,32 @@
+/* Author : Chaitaly Kundu
+Date: 18-04-21 */
+
+/* WAP to receive  Cartesian co-ordinates (x,y) of a point and convert them into polar co-ordinates (r,theta) */
+
+#include<bits/stdc++.h>
+#include<math.h>
+#define PI 3.14
+using namespace std;
+
+int main()
+{
+    float l1, l2, g1, g2, d;
+
+    cout << "Enter the Latitude in degrees: ";
+    cin >> l1 >> l2;
+
+    cout << "Enter the Longitude in degrees: ";
+    cin >> g1 >> g2;
+
+    l1 = l1 * PI / 180;
+    l2 = l2 * PI / 180;
+    g1 = g1 * PI / 180;
+    g2 = g2 * PI / 180;
+
+
+    d = 3963 * acos((sin (l1) * sin (l2))+ ( cos(l1) * cos(l2)) * cos(g2-g1));  // acos() is the method in math.h library for calculating cos inverse.
+
+
+    cout << "Distance between two places Nautical Mile is " << d;
+    return 0;
+}
