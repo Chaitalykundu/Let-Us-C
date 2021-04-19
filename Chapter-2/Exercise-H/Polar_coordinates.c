91d3202664aa58418f4504e2338e3f5febf06559
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-19 10:45:59 +0530

    Add commit

---
+++ Chapter-2/Exercise-H/Polar_coordinates.c
@@ -0,0 +1,23 @@
+/* Author : Chaitaly Kundu
+Date: 18-04-21 */
+
+/* WAP to receive  Cartesian co-ordinates (x,y) of a point and convert them into polar co-ordinates (r,theta) */
+
+#include<stdio.h>
+#include<math.h>
+#define PI 3.14
+
+int main()
+{
+    int x,y;
+    float r, theta;
+    printf("Enter the Cartesian Co-ordinates: ");
+    scanf("%d %d" ,&x,&y);
+
+    r=sqrt(pow(x,2)+pow(y,2));
+    theta= atan(y/x);  // atan() is the method in math.h library for calculating tan inverse.
+    theta = theta *180/ PI;
+
+    printf("Polar Co-ordinates (r , theta) = (%f , %f)" , r, theta);
+    return 0;
+}
