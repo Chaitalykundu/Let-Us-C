71db0f56e1ea13d37e125f3fb266ae3ad41c252a
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-24 10:31:52 +0530

    Add commit

--- Chapter-9/Example/address_3.c
+++ Chapter-9/Example/address_3.c
@@ -14,9 +14,9 @@ int main()
     int *j;  // It indicates that it will contain any adderss
     j=&i;
     printf("\n Address of i is %u", &i);   // Address of i (i.e. value of j)
-    printf("\n Value of j is %d",j);       // Value of j (i.e. address of i = &i)
     printf("\n Address of i is %u", j);   // Address (as j=&i;)
 
+    printf("\n Value of j is %d",j);       // Value of j (i.e. address of i = &i)
     printf("\n Address of j is %u", &j);   // Address of j
 
     printf("\n Value of i is %d",i);       // Value of i
