ec76db2c284c22fe9589000af4ee1397ba6992cb
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-26 09:34:16 +0530

    Add commit

---
+++ Chapter-10/Exercise-A/Recursion_a.c
@@ -0,0 +1,23 @@
+/* Author : Chaitaly Kundu
+Date : 26-04-21 */
+
+
+// Topic : Find the output
+
+
+#include<stdio.h>
+int main()
+{
+    printf("C to it that C survives");
+    main();
+    return 0;
+}
+
+
+/* Output: C to it that C survives
+           C to it that C survives
+           C to it that C survives
+           .
+           .
+           .
+           Infinity Loop */
---
+++ Chapter-10/Exercise-A/Recursion_b.c
@@ -0,0 +1,21 @@
+/* Author : Chaitaly Kundu
+Date : 26-04-21 */
+
+
+// Topic : Find the output
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int i=0;
+    i++;
+    if(i<=5)
+    {
+        printf("C adds wings to your thoughts\n");
+        exit(0);
+        main();
+    }
+    return 0;
+}
