37a6213e0ae3e99a36e7887cf9b6f52243aef2cb
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-04-30 18:31:37 +0530

    Add commit

---
+++ Chapter-8/Example/Sum_of_three_numbers.c
@@ -0,0 +1,21 @@
+/* Author: Chaitaly Kundu
+Date; 30-04-21 */
+
+/* WAP to add three numbers */
+
+#include<stdio.h>
+
+
+int add(int a, int b, int c)
+{
+    int sum = a + b+ c;
+    printf("%d\n", sum);
+}
+
+int main()
+{
+    int addition;
+    add(5,10,15);
+    return 0;
+}
+
