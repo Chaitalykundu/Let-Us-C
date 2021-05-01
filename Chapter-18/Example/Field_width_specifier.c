c70dfb09268c09fad1fabace515fad12d5287d31
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-01 11:28:54 +0530

    Add commit

---
+++ Chapter-18/Example/Field_width_specifier.c
@@ -0,0 +1,17 @@
+/*  Author : Chaitaly Kundu
+    Date: 1st May, 2021 */
+
+#include<stdio.h>
+
+int main()
+{
+    int a= 12;
+    printf("\nValue of a is %d",a);
+    printf("\nValue of a is %2d",a);
+    printf("\nValue of a is %4d",a);
+    printf("\nValue of a is %6d",a);
+    printf("\nValue of a is %-6d",a);
+    printf("\nValue of a is %-4d",a);
+
+    return 0;
+}
