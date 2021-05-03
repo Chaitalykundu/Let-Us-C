56d233a5cc66ecc2efe8169393d65b5ce0559b2a
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-03 21:59:05 +0530

    Add commit

---
+++ Chapter-18/Exercise-A/Console_a.c
@@ -0,0 +1,20 @@
+/*  Author : Chaitaly Kundu
+    Date: 3rd May, 2021 */
+
+/* Find out the output */
+
+#include<stdio.h>
+#include<ctype.h>
+
+int main()
+{
+    char ch;
+    ch = getchar();
+
+    if(islower(ch))
+        putchar(toupper((ch)));
+    else
+        putchar(tolower((ch)));
+
+    return 0;
+}
---
+++ Chapter-18/Exercise-A/Console_b.c
@@ -0,0 +1,22 @@
+/*  Author : Chaitaly Kundu
+    Date: 3rd May, 2021 */
+
+/* Find out the output */
+
+#include<stdio.h>
+int main()
+{
+    int i=2;
+    float f = 2.5367;
+    char str[] = "Life is like that";
+
+    printf("%4d\n%3.3f\n%4s\n", i, f, str);
+
+    return 0;
+}
+
+/* Output :
+   2
+2.537
+Life is like that
+*/
