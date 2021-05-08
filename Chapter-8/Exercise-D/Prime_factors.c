36b5ae43108ea57267a5ab1492f28c495b8fa3da
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-08 17:44:56 +0530

    Add commit

--- Chapter-8/Exercise-D/Prime_factors.c
+++ Chapter-8/Exercise-D/Prime_factors.c
@@ -20,7 +20,7 @@ int main()
     int n;
     printf("Enter the number: ");
     scanf("%d",&n);
-
+y
     Prime_Factor(n);
     return 0;
 }
--- Chapter-8/Exercise-D/Roman_equivalent.c
+++ Chapter-8/Exercise-D/Roman_equivalent.c
@@ -3,16 +3,56 @@
 
 #include<stdio.h>
 
-int roman(int n)
+void roman(int num)
 {
-
+    while(num)
+    {
+        if(num >= 1000)
+        {
+            printf("m");
+            num = num - 1000;
+        }
+        else if(num >= 500)
+        {
+            printf("d");
+            num = num - 500;
+        }
+        else if(num >= 100)
+        {
+            printf("c");
+            num = num - 100;
+        }
+        else if(num >= 50)
+        {
+            printf("l");
+            num = num - 50;
+        }
+        else if(num >= 10)
+        {
+            printf("x");
+            num = num - 10;
+        }
+        else if(num >= 5)
+        {
+            printf("v");
+            num = num - 5;
+        }
+        else if(num >= 1)
+        {
+            printf("i");
+            num = num - 1;
+        }
+    }
+    printf("\n");
 }
 
 int main()
 {
     int year;
-    printf("Enter the year: ");
-    scanf("%d",&year);
 
+      printf("Input the year to get its Roman Equivalent\n");
+      scanf("%d", &year);
+
+    roman(year);
     return 0;
 }
