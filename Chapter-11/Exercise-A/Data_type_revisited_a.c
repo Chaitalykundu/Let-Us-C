0102c8df4585ee36936c3aefc85ddf605000e4cf
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-16 11:52:59 +0530

    Add commit

---
+++ Chapter-11/Exercise-A/Data_type_revisited_a.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int i=0;
+void val();
+
+int main()
+{
+    printf("main's i = %d\n", i);
+    i++;
+    val();
+    printf("main's i = %d\n", i);
+    val();
+    return 0;
+}
+
+void val()
+{
+    i = 100;
+    printf("main's i = %d\n", i);
+    i++;
+}
+
+/* O/P:     main's i = 0
+            main's i = 100
+            main's i = 101
+            main's i = 100      */
