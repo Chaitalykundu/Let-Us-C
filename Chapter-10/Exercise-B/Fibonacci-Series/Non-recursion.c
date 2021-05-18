02023f20098bb555f0397349bc2b60df791c4ab2
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-18 11:53:23 +0530

    Add commit

--- Chapter-10/Exercise-B/Fibonacci-Series/Non-recursion.c
+++ Chapter-10/Exercise-B/Fibonacci-Series/Non-recursion.c
@@ -2,7 +2,7 @@
 Date : 18th May, 2021 */
 
 
-/* Write a program to find out the fibonacci series of first n numbers */
+/* Write a non-recursive function to find out the fibonacci series of first n numbers */
 
 
 #include<stdio.h>
