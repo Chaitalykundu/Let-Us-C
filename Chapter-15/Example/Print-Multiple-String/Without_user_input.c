c00857b34e6805df551cef8cae6f0857de538beb
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-19 10:45:05 +0530

    Add commit

---
+++ Chapter-15/Example/Print-Multiple-String/Without_user_input.c
@@ -0,0 +1,15 @@
+/* Author: CHaitaly Kundu
+Date: 19th May, 2021 */
+
+/* Write a program to print multiple string without user input*/
+
+#include<stdio.h>
+int main()
+{
+    char name[]= "Chaitaly Kundu";
+
+    puts("Hello");
+    puts(name);
+
+    return 0;
+}
