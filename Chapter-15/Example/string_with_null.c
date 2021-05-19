e510782964b82862287d945f50ac73c66dcc1a33
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2021-05-19 10:20:25 +0530

    Add commit

--- Chapter-15/Example/string_with_null.c
+++ Chapter-15/Example/string_with_null.c
@@ -1,17 +1,17 @@
 /* Author: CHaitaly Kundu
 Date: 5th dec, 2020 */
 
-/* Write a program to print a string / name */
+/* Write a program to print a string using NULL */
 
 #include<stdio.h>
 int main()
 {
     char name[]= "MONI";
     int i=0;
-    while(name!='\0')
+    while(name[i] !='\0')
     {
-            printf("%c",name[i]);
+        printf("%c",name[i]);
+        i++;
     }
-    i++;
     return 0;
 }
