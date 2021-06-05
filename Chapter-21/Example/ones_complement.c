6e1222e4b59119e41a598177c333d787e1381bf6
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-06-05 14:29:52 +0530

    Add commit

---
+++ Chapter-21/Example/ones_complement.c
@@ -0,0 +1,19 @@
+/*  Author : Chaitaly Kundu
+    Date: 5th June, 2021 */
+
+/* One's complement of a number */
+
+#include<stdio.h>
+
+int main()
+{
+    unsigned char ch=32;
+    unsigned char ones_comp = ~ch;
+
+    printf("%d\n",ones_comp);
+    printf("%x\n",ones_comp);
+    printf("%X\n",ones_comp);
+    printf("%#X\n",ones_comp);
+
+    return 0;
+}
