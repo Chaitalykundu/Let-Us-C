d66c4017bbe34bda511ef5ef9b7c02a9e18a3549
Chaitalykundu <chaitaly.slg01@gmail.com>
2021-07-11 02:00:18 +0530

    Add commit

--- Chapter-17/Extra/Student_details_using_global_variable.c
+++ Chapter-17/Extra/Student_details_using_global_variable.c
@@ -17,34 +17,42 @@ struct student ani , bishal, chandan;
 
 int main()
 {
+        // id Details
     ani.id = 1;
     bishal.id = 2;
     chandan.id = 3;
 
+        // mark Details
     ani.mark = 8.7;
     bishal.mark = 6.8;
     chandan.mark = 9.6;
 
+        // fav_character Details
     ani.fav_character = 'z';
     bishal.fav_character = 'y';
     chandan.fav_character = 'x';
 
+        // The string should be copied in char datatype
     strcpy(ani.full_name,"Ani Paul");
     strcpy(bishal.full_name,"Bishal Kumar");
     strcpy(chandan.full_name,"Chandan Roy");
 
+
+        // Ani's Detail
     printf("Ani's i no. %d\n", ani.id);
-    printf("Ani's full name is %s\n", ani.full_name);
+    printf("Ani's full name is %c\n", ani.full_name);
     printf("Ani got %.2f marks\n", ani.mark);
     printf("Ani's favourite character is %c\n",ani.fav_character);
     printf("\n\n");
 
-    printf("bishal's i no. %d\n", bishal.id);
-    printf("bishal's full name is %s\n", bishal.full_name);
-    printf("bishal got %.2f marks\n", bishal.mark);
-    printf("bishal's favourite character is %c\n", bishal.fav_character);
+        // Bishal's Detail
+    printf("Bishal's i no. %d\n", bishal.id);
+    printf("Bishal's full name is %s\n", bishal.full_name);
+    printf("Bishal got %.2f marks\n", bishal.mark);
+    printf("Bishal's favourite character is %c\n", bishal.fav_character);
     printf("\n\n");
 
+        // chandan's Detail
     printf("chandan's i no. %d\n", chandan.id);
     printf("chandan's full name is %s\n", chandan.full_name);
     printf("chandan got %.2f marks\n", chandan.mark);
